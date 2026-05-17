e4e924ad832b2a779b702c36999e96a6e538e648
ramya8812 <itzmeramyaarumugam@gmail.com>
2026-05-17 10:14:07 +0530

    first deployment

--- pillars/dev/backend.tf
+++ pillars/dev/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    # update the bucket name
     bucket         = "digi-dev-tf-s3-bucket"
     key            = "eks/dev/terraform.tfstate"
     region         = "us-east-1"
