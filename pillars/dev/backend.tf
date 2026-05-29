dbd69e8784bec79a2c3b58081e923d7a3285ebfc
Ramya2188 <itzmeramyaa@gmail.com>
2026-05-29 17:58:13 +0800

    Update backend.tf
    bucket name changed

--- pillars/dev/backend.tf
+++ pillars/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # update the state bucket name
-    bucket         = "digi-dev-tf-s3-bucket"
+    bucket         = "digi-dev-tf-s3-bucket-riya"
     key            = "eks/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "digi-dev-tf-lock-state"
