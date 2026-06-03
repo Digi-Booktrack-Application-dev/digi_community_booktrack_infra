54eaf22921ab07cb5aed39c98d0461f1c57d5db8
ramya8812 <itzmeramyaarumugam@gmail.com>
2026-06-03 14:55:06 +0800

    Initial commit

---
+++ pillars/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    # update the state bucket name
+    bucket         = "digi-dev-tf-s3-bucket"
+    key            = "eks/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "digi-dev-tf-lock-state"
+  }
+}
